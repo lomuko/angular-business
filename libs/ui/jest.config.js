1d478fb1da5437217fe5cce8cc8d87e9d823bde5
Alberto Basalo <albertobasalo@hotmail.com>
2019-03-25 12:29:03 +0100

    ng generate @nrwl/schematics:library ui --inlineStyle

---
+++ libs/ui/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'ui',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/libs/ui',
+  snapshotSerializers: ['jest-preset-angular/AngularSnapshotSerializer.js', 'jest-preset-angular/HTMLCommentSerializer.js']
+};
