d44cd8470c260f7fcce277a8b178f8b4772fc1b7
Alberto Basalo <albertobasalo@hotmail.com>
2019-04-12 17:40:13 +0200

    test WIP testing pipe injected by http

--- apps/shop/src/app/shared/ex-rate.pipe.spec.ts
+++ apps/shop/src/app/shared/ex-rate.pipe.spec.ts
@@ -1,8 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { ExRatePipe } from './ex-rate.pipe';
-
+class MockHttpClient extends HttpClient {
+  constructor() {
+    super(null);
+  }
+}
 describe('ExRatePipe', () => {
   it('create an instance', () => {
-    const pipe = new ExRatePipe();
+    const pipe = new ExRatePipe(new MockHttpClient());
     expect(pipe).toBeTruthy();
   });
 });
--- apps/warehouse/src/app/shared/out-of-stock.directive.spec.ts
+++ apps/warehouse/src/app/shared/out-of-stock.directive.spec.ts
@@ -1,8 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { ElementRef } from '@angular/core';
+import { async, TestBed } from '@angular/core/testing';
 import { OutOfStockDirective } from './out-of-stock.directive';
 
+class MockElementRef extends ElementRef {
+  constructor() {
+    super(null);
+  }
+}
+
 describe('OutOfStockDirective', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [OutOfStockDirective],
+      imports: [CommonModule]
+    }).compileComponents();
+  }));
   it('should create an instance', () => {
-    const directive = new OutOfStockDirective();
+    const directive = new OutOfStockDirective(new MockElementRef());
     expect(directive).toBeTruthy();
   });
 });
